275ed9789c6d308ec84a5b41edff83fabb7c442c
dieppa <hm.dieppa@gmail.com>
2022-12-18 23:29:34 +0100

    Added standard Android lifecycle state and effect collector

--- omni-mvi-android/src/main/java/net/asere/omni/mvi/StateContainerHostExtension.kt
+++ omni-mvi-android/src/main/java/net/asere/omni/mvi/StateContainerHostExtension.kt
@@ -7,7 +7,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.launch
 
@@ -28,7 +31,7 @@ fun <Effect> StateContainerHost<*, Effect>.OnEffect(
 }
 
 @Composable
-fun <UiState, Effect> StateContainerHost<UiState, Effect>.state(
+fun <UiState> StateContainerHost<UiState, *>.state(
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
 ): State<UiState> {
     val lifecycleOwner = LocalLifecycleOwner.current
@@ -36,4 +39,28 @@ fun <UiState, Effect> StateContainerHost<UiState, Effect>.state(
         container.asStateContainer().state.flowWithLifecycle(lifecycleOwner.lifecycle, minActiveState)
     }
     return lifecycleStateFlow.collectAsState(currentState)
+}
+
+fun <UiState> StateContainerHost<UiState, *>.observeState(
+    lifecycleOwner: LifecycleOwner,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    onState: (suspend (state: UiState) -> Unit)
+) {
+    lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(lifecycleState) {
+            launch { container.asStateContainer().state.collect { onState(it) } }
+        }
+    }
+}
+
+fun <Effect> StateContainerHost<*, Effect>.observeEffect(
+    lifecycleOwner: LifecycleOwner,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    onEffect: (suspend (effect: Effect) -> Unit)
+) {
+    lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(lifecycleState) {
+            launch { container.asStateContainer().effect.collect { onEffect(it) } }
+        }
+    }
 }
