41392c59c8b3732533004ea2da609b87385fb2da
dieppa <hm.dieppa@gmail.com>
2023-11-12 21:25:20 +0100

    Deprecated postEffect and postState extension functions.

--- omni-mvi/src/main/java/net/asere/omni/mvi/IntentScope.kt
+++ omni-mvi/src/main/java/net/asere/omni/mvi/IntentScope.kt
@@ -7,12 +7,24 @@ class IntentScope<State, Effect>(
     val container: StateContainer<State, Effect>,
 ) : ExecutionScope()
 
+@Deprecated("Use reduce extension function", ReplaceWith("reduce(function)"))
 @OmniHostDsl
 fun <State> IntentScope<State, *>.postState(
     function: State.() -> State
 ) = container.update(function)
 
 @OmniHostDsl
+fun <State> IntentScope<State, *>.reduce(
+    function: State.() -> State
+) = container.update(function)
+
+@Deprecated("Use emit extension function", ReplaceWith("emit(Effect)"))
+@OmniHostDsl
 fun <Effect> IntentScope<*, Effect>.postEffect(
+    effect:  Effect
+) = container.post(effect)
+
+@OmniHostDsl
+fun <Effect> IntentScope<*, Effect>.emit(
     effect:  Effect
 ) = container.post(effect)
